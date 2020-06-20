ee968978d6c57695b5c8e4bb4c3548062f680f93
nicolasmorales1 <nicolasmorales@uninorte.edu.co>
2020-06-20 16:39:41 -0500

    test

--- src/components/menu/menu_item.js
+++ src/components/menu/menu_item.js
@@ -1,37 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { storage } from '../../constants/firebase'
-import { Grid, Card, CardContent, CardActionArea, CardMedia } from '@material-ui/core'
-import './menu.scss'
+import React, { useState, useEffect } from "react";
+import { storage } from "../../constants/firebase";
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardActionArea,
+  CardMedia,
+} from "@material-ui/core";
+import "./menu.scss";
 
 function MenuItem(props) {
-  const [imageUrl, setImageUrl] = useState('https://image.shutterstock.com/image-vector/burger-hamburger-logo-icon-design-260nw-1445463893.jpg')
+  const [imageUrl, setImageUrl] = useState(
+    "https://image.shutterstock.com/image-vector/burger-hamburger-logo-icon-design-260nw-1445463893.jpg"
+  );
 
-  useEffect(
-    () => {
-      storage.child(props.item.image_url).getDownloadURL().then((url) => {
-        setImageUrl(url)
-      }).catch((error) => {
+  useEffect(() => {
+    storage
+      .child(props.item.image_url)
+      .getDownloadURL()
+      .then((url) => {
+        setImageUrl(url);
+      })
+      .catch((error) => {
         // Handle any errors
-      }) 
-    }, []
-  )
+      });
+  }, []);
 
   return (
-  <Grid item>
-    <Card className="card_menu_item">
-      <CardActionArea>
-        <CardMedia
-          className="media_menu_item"
-          image={imageUrl}
-        />
-        <CardContent>
-          <h1>{props.item.name}</h1>
-          <p>{props.item.description}</p>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  </Grid>
-  )
+    <Grid item>
+      <Card className="card_menu_item">
+        <CardActionArea>
+          <CardMedia className="media_menu_item" image={imageUrl} />
+          <CardContent>
+            <h1>{props.item.name}</h1>
+            <p>{props.item.description}</p>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 }
 
-export default MenuItem
+export default MenuItem;
