256e431b8852422d97b10c9b6ebeee936e0259f9
Mrls94 <sebasmrls94@gmail.com>
2020-06-20 13:07:08 -0500

    Initial project

---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { menu } from '../constants/menu'
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+export default Welcome;
---
+++ src/constants/menu.js
@@ -0,0 +1,13 @@
+export const menu = [
+  {
+    name: 'Hamburguesa Sweetbacon',
+    description: '160g de carne, queso + cheddar, mermelada de tocineta ahumada, cebollas caramelizadas en salsa de barbacoa a base de cerveza negra y panela',
+    price: '$20k',
+    image_url: 'sweetbaconcroped.jpeg'
+  },
+  {
+    name: 'Sandwich de Pulled Pork',
+    description: '160 g de costilla de cerdo. Cocinadas a fuego lento Pulled Pork, queso + cheddar, cebollas caramelizadas en salsa barbacoa',
+    image_url: 'pulledporkcroped.jpeg'
+  }
+]
